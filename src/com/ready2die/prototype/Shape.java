dd7f99c87d16eb7d62209126743c46df097a978b
Leesunghee <realwunz@gmail.com>
2018-08-12 19:29:04 +0900

    add prototype pattern example

---
+++ src/com/ready2die/prototype/Shape.java
@@ -0,0 +1,14 @@
+package com.ready2die.prototype;
+
+public class Shape implements Cloneable {
+
+    private String id;
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+}
