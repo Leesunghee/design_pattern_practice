d963958f406f37dabc04bb3cf1e1b587d5b302dc
Leesunghee <realwunz@gmail.com>
2018-09-22 13:21:33 +0900

    intellij files delete

--- src/com/ready2die/factoryMethod/concrete/Text.java
+++ src/com/ready2die/factoryMethod/concrete/Text.java
@@ -1,4 +1,24 @@
 package com.ready2die.factoryMethod.concrete;
 
+import java.io.BufferedReader;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+
 public class Text {
+
+    public static void main(String[] args) {
+        String str = "Text";
+        String str2 = "text2";
+
+        String test ="text2";
+
+        //language=JSON
+        String json = "{\n" +
+                "\"name\": \"lee\"}";
+        try {
+            BufferedReader bufferedReader = new BufferedReader(new FileReader("new.txt"));
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        }
+    }
 }
